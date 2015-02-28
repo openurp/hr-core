a40d313599307802261234012b4d1d49771d6491
chaostone <duantihua@gmail.com>
2015-02-28 22:36:05 +0800

    first import

---
+++ base/ds/src/main/scala/org/openurp/hr/base/ds/code/AbstractWS.scala
@@ -0,0 +1,26 @@
+package org.openurp.hr.base.ds.code
+
+import org.beangle.data.jpa.dao.OqlBuilder
+import org.beangle.data.model.dao.EntityDao
+import org.beangle.webmvc.api.action.EntityActionSupport
+import org.beangle.webmvc.api.annotation.response
+import org.openurp.code.BaseCode
+import org.beangle.data.model.Entity
+import org.beangle.commons.http.accept.ContentNegotiationManager
+import org.beangle.webmvc.entity.action.RestfulService
+
+class AbstractWS[T <: BaseCode] extends RestfulService[T] {
+
+  @response
+  override def index(): Any = {
+    val builder = OqlBuilder.from(entityType, "code")
+    builder.orderBy(get("orderBy", "code.code"))
+    buildQuery(builder)
+    if (this.isRequestCsv) put("properties", List(classOf[BaseCode] -> List("id", "code","name"), classOf[Entity[_]] -> List("id")))
+    entityDao.search(builder)
+  }
+
+  def buildQuery(builder: OqlBuilder[T]): Unit = {
+
+  }
+}
---
+++ base/ds/src/main/scala/org/openurp/hr/base/ds/code/school.scala
@@ -0,0 +1,14 @@
+package org.openurp.base.ds.code
+
+import org.openurp.hr.base.ds.code.AbstractWS
+import org.openurp.hr.base.code.TeacherType
+import org.openurp.hr.base.code.TutorType
+import org.openurp.hr.base.code.WorkState
+
+class TeacherStateWS extends AbstractWS[WorkState]
+
+class TeacherTypeWS extends AbstractWS[TeacherType]
+
+class TutorTypeWS extends AbstractWS[TutorType]
+
+
